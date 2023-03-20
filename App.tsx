1f265e76bc897618ba4f3a59d0e28b2781e408bb
AV <angel@viera.tech>
2023-03-20 13:22:07 +0800

    first demo works

--- App.tsx
+++ App.tsx
@@ -1,118 +1,74 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, {Component} from 'react';
+import {View, Text, TouchableOpacity} from 'react-native';
+import HealthKit, { HKUnit, HKQuantityTypeIdentifier, HKCategoryTypeIdentifier, HKUnits } from '@kingstinct/react-native-healthkit';
+import Toast from 'react-native-toast-message';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+type AppState = {};
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+class App extends Component<{}, AppState> {
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  componentDidMount() {
+    this.requestHealthKitAuthorization();
+  }
 
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  async handlePressGetAuthStatus() {
+    const isAvailable = await HealthKit.isHealthDataAvailable();
+    /* Read latest sample of any data */
+    const result = await HealthKit.authorizationStatusFor(HKQuantityTypeIdentifier.bodyFatPercentage);
+    console.log(`HealthKit Auth Status: ${result}`)
+    Toast.show({text1: `HealthKit Auth Status: ${result}`, type: 'success'});
   };
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
+  async requestHealthKitAuthorization() {
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+    const isAvailable = await HealthKit.isHealthDataAvailable();
+
+    if (!isAvailable) {;
+      console.log('HealthKit is not available on this device!')
+      Toast.show({text1: `HealthKit is not available on this device!`, type: 'error'});
+      return;
+    }
+
+    await HealthKit.requestAuthorization([HKQuantityTypeIdentifier.bodyFatPercentage]); // request read permission for bodyFatPercentage
+  }
+
+  async exportHistoricalHealthData() {
+    const result = await HealthKit.getMostRecentQuantitySample(HKQuantityTypeIdentifier.bodyFatPercentage, HKUnits.Percent); // read latest sample
+    const json_result = JSON.stringify(result);
+    console.log(result?.quantity);
+    Toast.show({text1: `Last BF%: ${result?.quantity}`, type: 'info'});
+  }
+
+  render() {
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text>HealthKit Step Data Exporter</Text>
+        <TouchableOpacity
+          onPress={this.handlePressGetAuthStatus.bind(this)}
+          style={{
+            backgroundColor: '#4CAF50',
+            paddingHorizontal: 20,
+            paddingVertical: 10,
+            borderRadius: 5,
+            marginTop: 20,
+          }}>
+          <Text style={{color: 'white'}}>Healthkit Auth Status</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={this.exportHistoricalHealthData.bind(this)}
+          style={{
+            backgroundColor: '#4CAF50',
+            paddingHorizontal: 20,
+            paddingVertical: 10,
+            borderRadius: 5,
+            marginTop: 20,
+          }}>
+          <Text style={{color: 'white'}}>Get Last Body Fat %</Text>
+        </TouchableOpacity>
+        <Toast />
+      </View>
+    );
+  }
+}
 
 export default App;
